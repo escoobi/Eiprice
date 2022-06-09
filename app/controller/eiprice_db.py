76f95539bcb80d331fbfae68eedbefea9e3ddbfb
escoobi <gustavo_o_@hotmail.com>
2022-06-09 16:17:32 -0400

    Eiprice - Final

--- app/controller/eiprice_db.py
+++ app/controller/eiprice_db.py
@@ -3,6 +3,22 @@ import psycopg2
 
 caminho_db = 'postgresql://postgres:123159@localhost/eiprice'
 
+""" sql_criar_banco
+-- Database: eiprice
+-- DROP DATABASE IF EXISTS eiprice;
+
+CREATE DATABASE eiprice
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;"
+    
+"""
+
+
 
 def conectar():
   conexao_pg = psycopg2.connect(host='localhost', user='postgres', password='123159', database='eiprice', port=5432)  
